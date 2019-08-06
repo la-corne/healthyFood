adc0d46335f5ed319810d6823c700a8469de161a
Sara <you@example.com>
2019-08-06 06:57:31 +0200

    fixing the background image

--- healthy_food/app/controllers/welcome_controller.rb
+++ healthy_food/app/controllers/welcome_controller.rb
@@ -1,10 +1,39 @@
-class WelcomeController<ApplicationController
+class WelcomeController < ApplicationController
+  before_action :flag_navigation_bar, :only => [:contact_us, :home, :profile, :saved, :service]
+
+
   def login
 
   end
 
   def signup
 
+  end
+
+  def contact_us
+  end
+
+
+  def home
+  end
+
+
+  def profile
+  end
+
+
+  def saved
+  end
+
+
+  def service
+  end
+
+
+  private
+
+  def flag_navigation_bar
+    @navigation_bar = true
   end
 
 end
